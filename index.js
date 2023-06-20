4f63b20d9ac4f06ea2950f9fe58d8bb11e7e636e
grace-syingali <135760083+grace-syingali@users.noreply.github.com>
2023-06-20 10:14:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let friends = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(friends, event) {
+    for(let i = 0; i < friends.length; i++) {
+        console.log (`"Thank you, ${friends[i]}, for the wonderful ${event} gift!"`)
+    }
+
+    return friends
+}
+writeCards(friends, "surprise")
+
+ function countDown() {
+    let number = 10;
+    while (number < 10) {
+      console.log (number--)
+    }
+ }
